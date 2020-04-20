18aaf1e2a5c93846c49620d0128978a3dee00d43
ergerica <ergerica76@gmail.com>
2020-04-20 20:45:27 +0200

    Initial Commit

---
+++ ProjectChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectChallenge
+//
+//  Created by Erica Ross on 4/20/20.
+//  Copyright © 2020 Erica Ross. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
